8f09e277abdf232ac0492e8fc0ec48aea94fbd9f
NicoLanders <nlandero23@gmail.com>
2025-02-17 16:29:11 -0300

    commit 4: ajustes de variables de entorno

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 const reservasRutas = require('./src/rutas/reservasRutas.js')
 
+require('dotenv').config();
+
+const port = process.env.PORT ||3000
+const serverUrl = process.env.SERVER_URL || `http://localhost:${port}`
+
 app.use(express.json())
 app.use(express.urlencoded({
     extended: true
@@ -9,7 +14,7 @@ app.use(express.urlencoded({
 
 app.use('/api', reservasRutas)
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Puerto conectado')
 })
 
--- src/rutas/reservasRutas.js
+++ src/rutas/reservasRutas.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const rutas = express.Router()
-const reservaControlador = require ('../controladores/reservaControlador.js')
+const reservaControlador = require('../controladores/reservaControlador.js')
 
 rutas.post('/', reservaControlador.crearReserva)
 rutas.get('/reserva/:id', reservaControlador.obtenerReservaId)
